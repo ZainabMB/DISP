bafcffc5f6c563e97f16f98c9ed55452506e176a
vasi-m <148768787+vasi-m@users.noreply.github.com>
2026-05-05 15:26:01 +0100

    Update SendOrderDetailsService.java

--- Automation/src/main/java/com/disp/automation/service/SendOrderDetailsService.java
+++ Automation/src/main/java/com/disp/automation/service/SendOrderDetailsService.java
@@ -1,4 +1,27 @@
 package com.disp.automation.service;
 
+import io.camunda.client.CamundaClient;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
 public class SendOrderDetailsService {
+
+    private final CamundaClient camundaClient;
+
+    public SendOrderDetailsService(CamundaClient camundaClient) {
+        this.camundaClient = camundaClient;
+    }
+
+    public void sendOrderDetails(String orderId, Map<String, Object> vars) {
+
+        camundaClient
+                .newPublishMessageCommand()
+                .messageName("orderDetails")
+                .correlationKey(orderId)
+                .variables(vars)
+                .send()
+                .join();
+    }
 }
